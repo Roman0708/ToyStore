5af5987f4004a03c2e5a6918b4ad4e297fdaa9ef
Roman_Kalimov <rrkalimov@gmail.com>
2024-05-25 11:08:26 +0300

    getToy fixed

--- Model/Store.java
+++ Model/Store.java
@@ -3,14 +3,14 @@ package Model;
 import java.util.*;
 
 public class Store {
-    private List<Toy> toys = new List<Toy>();
+    private List<Toy> toys = new ArrayList<Toy>();
 
     public void newToy(Toy toy) {
         toys.add(toy);
     }
 
     public Toy getToy(int id) {
-        if (id >= 0 && toys.contains(toys[id])) 
+        if (id >= 0 && toys.contains(toys.get(id))) 
             return toys.get(id);
         return null;
     }
